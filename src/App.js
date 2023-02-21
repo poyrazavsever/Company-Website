4d5171a1c0d9ae46a38ef2b7098b9bddce96a355
Poyraz Avsever <poyrazavsever@hotmail.com>
2023-02-21 20:06:47 +0300

    feat: file and route layout done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+
+//Pages
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Login from "./pages/Login"
+import Portfolio from "./pages/Portfolio"
+import Repos from "./pages/Repos"
 
 // Components 
 import SidebarMenu from "./components/SidebarMenu";
@@ -7,6 +17,16 @@ function App() {
   return (
     <div>
       <SidebarMenu />
+      <ToastContainer />
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/portfolio" element={<Portfolio />}/>
+        <Route path="/repos" element={<Repos />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Repos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Repos() {
+  return (
+    <div>Repos</div>
+  )
+}
+
+export default Repos
