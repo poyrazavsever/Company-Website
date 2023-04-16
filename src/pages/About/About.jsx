fe16f7f36bb2be6969d8b628a8064e49a2b235da
Poyraz Avsever <poyrazavsever@hotmail.com>
2023-04-16 21:35:18 +0300

    feat: about done

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,11 +6,11 @@ import TimelineSec from './Timeline'
 
 function About() {
   return (
-    <>
+    <div className='animate-sliderStart'>
       <Banner />
 
       <TimelineSec />
-    </>
+    </div>
 
   )
 }
